74f93123bb4da537ae9aee92f3519cfa18871864
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2022-03-01 15:57:37 +0800

    Require WC 3.9.4+

--- woocommerce-product-sku-generator.php
+++ woocommerce-product-sku-generator.php
@@ -18,7 +18,7 @@
  * @copyright Copyright (c) 2014-2022, SkyVerge, Inc. (info@skyverge.com)
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  *
- * WC requires at least: 3.0.9
+ * WC requires at least: 3.9.4
  * WC tested up to: 4.1.0
  */
 
@@ -48,7 +48,7 @@ class WC_SKU_Generator {
 	const VERSION = '2.4.6-dev.1';
 
 	/** required WooCommerce version number */
-	const MIN_WOOCOMMERCE_VERSION = '3.0.9';
+	const MIN_WOOCOMMERCE_VERSION = '3.9.4';
 
 	/** @var WC_SKU_Generator single instance of this plugin */
 	protected static $instance;
